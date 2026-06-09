eb78b755d888f3456af7d0391b66f9780da73de6
Tejdeep Singh <tejdeepsingh18@gmail.com>
2026-06-09 13:57:50 +0530

    fix stats route

--- server/routes/statsRoutes.js
+++ server/routes/statsRoutes.js
@@ -1,94 +1,47 @@
-const express =
-require("express");
+const express = require("express");
 
-const router =
-express.Router();
+const router = express.Router();
 
-const SpeedTest =
-require("../models/SpeedTest");
+const SpeedTest = require("../models/SpeedTest");
 
-router.post(
+router.get(
+  "/average",
+  async (req, res) => {
+    try {
 
-"/",
+      const stats = await SpeedTest.aggregate([
+        {
+          $group: {
+            _id: null,
+            avgDownload: { $avg: "$download" },
+            avgUpload: { $avg: "$upload" },
+            avgPing: { $avg: "$ping" }
+          }
+        }
+      ]);
 
-async(
+      if (stats.length === 0) {
+        return res.json({
+          avgDownload: 0,
+          avgUpload: 0,
+          avgPing: 0
+        });
+      }
 
-req,
-res
+      res.json({
+        avgDownload: Number(stats[0].avgDownload.toFixed(2)),
+        avgUpload: Number(stats[0].avgUpload.toFixed(2)),
+        avgPing: Number(stats[0].avgPing.toFixed(2))
+      });
 
-)=>{
+    } catch (err) {
 
-try{
+      res.status(500).json({
+        message: err.message
+      });
 
-const {
-
-lat,
-lng,
-radius
-
-}
-
-=
-
-req.body;
-
-const tests =
-
-await SpeedTest.find({
-
-location:{
-
-$near:{
-
-$geometry:{
-
-type:"Point",
-
-coordinates:[
-
-lng,
-
-lat
-
-]
-
-},
-
-$maxDistance:
-
-radius
-
-}
-
-}
-
-});
-
-res.json(
-
-tests
-
+    }
+  }
 );
 
-}
-
-catch(err){
-
-res.status(500)
-
-.json({
-
-message:
-
-err.message
-
-});
-
-}
-
-}
-
-);
-
-module.exports=
-router;
+module.exports = router;
